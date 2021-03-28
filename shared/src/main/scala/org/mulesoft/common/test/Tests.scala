7ece7eebb1a8a613b10218dfae1b88ddc41d07fe
Nicolas Schejtman <nschejtman93@gmail.com>
2021-03-28 11:22:16 -0300

    Add golden overriding without computing diffs

--- shared/src/main/scala/org/mulesoft/common/test/Tests.scala
+++ shared/src/main/scala/org/mulesoft/common/test/Tests.scala
@@ -7,7 +7,7 @@ import org.scalatest.{Assertion, Succeeded}
 import org.scalatest.Matchers._
 
 import java.io.{File, FileNotFoundException, FileReader, Reader}
-import java.lang.System.getProperty
+import java.lang.System.{arraycopy, getProperty}
 import java.net.{InetAddress, UnknownHostException}
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.Random
@@ -19,23 +19,41 @@ object Tests {
 
   private implicit val executionContext: ExecutionContext = ExecutionContext.Implicits.global
 
-  def checkLinesDiff(a: AsyncFile, e: AsyncFile, encoding: String = Utf8): Future[Assertion] = computeDiff(a, e, Diff.caseSensitive, encoding)
+  def checkLinesDiff(a: AsyncFile, e: AsyncFile, encoding: String = Utf8): Future[Assertion] =
+    computeDiff(a, e, Diff.caseSensitive, encoding)
 
-  def checkDiff(a: AsyncFile, e: AsyncFile, encoding: String = Utf8): Future[Assertion] = computeDiff(a, e, Diff.ignoreAllSpace, encoding)
+  def checkDiff(a: AsyncFile, e: AsyncFile, encoding: String = Utf8): Future[Assertion] =
+    computeDiff(a, e, Diff.ignoreAllSpace, encoding)
 
   def computeDiff(a: AsyncFile, e: AsyncFile, differ: Diff.Str, encoding: String = Utf8): Future[Assertion] = {
     a.read(encoding).zip(e.read(encoding)).map {
       case (actual, expected) =>
-        val diffs = differ.diff(actual.toString, expected.toString)
-        if (diffs.nonEmpty) {
-          if (goldenOverride) {
-            a.read(encoding).map(content => e.write(content.toString, encoding))
-          } else {
-            fail(s"\ndiff -y -W 150 $a $e\n\n${makeString(diffs)}")
-          }
+        lazy val diffs = differ.diff(actual.toString, expected.toString)
+        if (goldenOverrideAll) {
+          overwrite(a, e, encoding) // succeeds
+        }
+        else if (diffs.nonEmpty && (goldenOverride || goldenOverrideDifferent)) {
+          overwrite(a, e, encoding) // succeeds
+        }
+        else if (diffs.nonEmpty) {
+          fail(s"\ndiff -y -W 150 $a $e\n\n${makeString(diffs)}")
         }
         succeed
     }
+  }
+
+  /**
+    *  Overwrites expected file with actual file's contents
+    * @param actual file
+    * @param expected file
+    * @param encoding encoding for both reading actual file and writing expected file
+    * @return Unit -> This is IO
+    */
+  private def overwrite(actual: AsyncFile, expected: AsyncFile, encoding: String): Future[Unit] = {
+    for {
+      content <- actual.read(encoding)
+      _       <- expected.write(content.toString, encoding)
+    } yield {}
   }
 
   def checkDiff(tuple: (String, String)): Assertion = tuple match {
@@ -52,7 +70,13 @@ object Tests {
     }
   }
 
-  /** Force golden override. */
+  @deprecated("Use goldenOverrideDifferent instead", since = "0.0.4")
   private def goldenOverride: Boolean = Option(getProperty("golden.override")).isDefined
+
+  /** Overrides goldens which have differences */
+  private def goldenOverrideDifferent: Boolean = Option(getProperty("golden.override.different")).isDefined
+
+  /** Overrides all goldens without computing differences */
+  private def goldenOverrideAll: Boolean = Option(getProperty("golden.override.all")).isDefined
 
 }
